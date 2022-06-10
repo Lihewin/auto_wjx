2893a1898b934f1937258b051f0cef827ec496ed
ivory <510485871@qq.com>
2022-06-11 07:43:05 +0800

    Commit Integrated revision.

--- README.md
+++ README.md
@@ -1,28 +1,120 @@
-# 不会真有人连问卷星都要弄虚作假吧
-## 仅支持多选题，单选题，以不放回的方式随机选择
-形式主义既然是形式，那就试着形式地自动化
+# Auto WJX
 
-python+selenium，虽不优雅，但省时有效
+### 作者留言：
 
-我相信会用自动化的宝贝们没人会在真正重要的作业上搞自动化吧。
+- 不会真有人，连问卷星都要弄虚作假吧。
+- 形式主义既然是形式，那就试着形式地自动化。
+- 我相信会用自动化的宝贝们，没人会在真正重要的作业上搞自动化吧。
+- 仅支持多选题，单选题，以不放回的方式随机选择
+- python + selenium，虽不优雅，但省时有效
+- ~~954-ivory 的回复：帮女票弄的，顺手给你改了些东西，现在应该优雅了点。~~
+
+### 教程 - Tutorial
+
+#### 配置教程（Bing 能搜出一堆教程，以下仅供参考）
+
+1. 安装 [python](https://www.python.org/downloads/) （安装完毕，可用以下命令查询 python 安装路径）
 
 ```python
-choose_one(题号,[概率1,概率2,...])
-choose_multiple(题号,[概率1,概率2,...],最多选项限制（可选参数）)
+from os import path
+
+print(path)  # Windows 下，在 Python 命令行中可直接打印当前 path。
 ```
-首先安装依赖
-```
+
+2. 利用 pip 安装项目依赖
+
+```shell
 pip install -r requirements.txt
 ```
 
-chromedriver记得替换成自己系统的版本，并在以下句子中更改路径
+3. 下载
+
+- 谷歌浏览器
+  [Chrome Driver](http://chromedriver.storage.googleapis.com/index.html) ,
+  [Chrome Driver 淘宝镜像](https://registry.npmmirror.com/binary.html?path=chromedriver/)
+- 火狐浏览器 [Gecko Driver](https://github.com/mozilla/geckodriver/releases)
+- IE 浏览器 [IE Driver](http://selenium-release.storage.googleapis.com/index.html)
+
+4. 在 `wjx.py` 中配置 Driver
 
 ```python
-driver = webdriver.Chrome(executable_path=r'./chromedriver', options=opt)
+# wjx.py 
+
+driver = webdriver.Chrome(options=opt)  # Windows下，将对应版本的 chromedriver 放置在 python 根目录，默认启用该行。
+
+# ------或者 -------
+
+driver = webdriver.Chrome(executable_path=r'./chromedriver', options=opt)  # 该行已被注释
+
 ```
 
-大概连续提交三十次左右会触发滑动验证码，换ip可解，也有解决验证码的代码，但我没跑起来。
+#### 示例用法
 
-我一个晚上跑了两百多份，我感觉差不多够了。
+1. 修改 `conf.py` 中的配置项
 
+```python
+# conf.py
 
+# 以配置项执行
+QUESTION_URL = "https://www.wjx.cn/vm/wE5Js0M.aspx"  # 问卷地址
+LOOP_COUNT = 1  # 执行次数
+
+# ------或者-------
+
+# 以命令行交互
+QUESTION_URL = None
+LOOP_COUNT = None
+```
+
+2. 修改 `wjx.py` 中的 `choose_answer()` 方法
+
+```python
+# wjx.py
+
+# 为方便非计算机专业朋友们食用，以下示例以伪代码的形式给出，请自行替换参数。
+choose_one(题号, [概率1, 概率2, ...])
+choose_multiple(题号, [概率1, 概率2, ...], 最多选项限制（可选参数）)
+```
+
+3. 利用 `cmd` 执行项目脚本
+
+```shell
+cd /path/to/auto_wjx # 这里的 /path/to/auto_wjx 替换成你本机的文件夹路径
+python wjx.py
+```
+
+---
+
+### 更新日志 - Update log
+
+#### 2022-06-11
+
+_以下示例以 `choose_one` 为例，`choose_multiple` 同理。_
+
+1. 实现了相同概率选项的自动填充：
+
+```python
+# wjx.py
+
+choose_one(1, [0.25, 0.25, 0.25, 0.25])  # 之前的做法。
+choose_one(1)  # 各项均为相同概率，可省略不写。
+```
+
+2. 新增排除某些选项的支持（目前仅支持上述自动填充的写法）：
+
+```python
+# wjx.py
+
+choose_one(1, exclude=[1])  # 排除第 1 题中，第 1 个选项。
+choose_one(1, exclude=[1, 2, 3])  # 排除第 1 题中，第 1, 2, 3 个选项。 
+
+choose_one(1, [0, 0.4, 0.6])  # 各项概率不相同，则没必要用 `exclude`，直接将排除项置 0。
+```
+
+3. 实现了右滑验证码逃逸（默认窗口大小应该能够逃逸，有问题请自行调教参数 dest）。
+4. 用 Python 层的实现，替代了 `circle.sh`，降低了使用门槛。
+
+### TODO
+
+- 新增利用 `conf.py` 来配置选中项的功能。
+- 实现其他类型 `input` 的自动填写功能。
--- wjx.py
+++ wjx.py
@@ -1,66 +1,143 @@
-from lib2to3.pgen2 import driver
+import logging
 import random
-from time import time
 from selenium import webdriver
+from selenium.common import TimeoutException, NoSuchElementException, ElementClickInterceptedException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver import ActionChains
 import numpy as np
 import time
 
-opt = webdriver.ChromeOptions()
-opt.add_experimental_option('excludeSwitches', ['enable-automation'])
-opt.add_experimental_option('useAutomationExtension', False)
-driver = webdriver.Chrome(executable_path=r'./chromedriver', options=opt)
-driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',
-                           {'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'})
+import conf
 
-np.random.seed(int(time.time()))
 
-def choose_one(question_number, question_probabilty):
-    number_of_choices = len(driver.find_elements(By.XPATH,f"//*[@id=\"div{question_number}\"]/div[2]/div"))
-    chosen_number = np.random.choice(list(range(1,number_of_choices+1)),p = question_probabilty)
-    driver.find_element(By.XPATH,f"//*[@id=\"div{question_number}\"]/div[2]/div[{chosen_number}]").click()
+def choose_answer():
+    try:
+        choose_one(1)  # 各项均为相同概率，可省略不写。
+        choose_one(2, [0.1, 0.2, 0.5, 0.2])
+        choose_one(3, exclude=[2, 3])  # 排除第 3 题中，第 2, 3 个选项。
+        choose_one(4, [0.4, 0.1, 0.2, 0.1, 0.2])
+        choose_one(5, [0.2, 0.2, 0.1, 0.3, 0.2])
+        choose_one(6, [0.25, 0.25, 0.25, 0.25])
+        choose_one(7, [0.3, 0.2, 0.1, 0.4])
+        choose_one(8, [0.2, 0.6, 0.2])
+        choose_multiple(9, [0.3, 0.25, 0.2, 0.125, 0.125], 3)
+        choose_multiple(10, [0.3, 0.25, 0.2, 0.15, 0.1])
+        choose_one(11, [0.1, 0.1, 0.2, 0.6])
+        choose_one(12, [0.2, 0.8])
+        choose_multiple(13, [0.4, 0.2, 0.3, 0.1], 2)
+        choose_multiple(14, [0.3, 0.3, 0.3, 0.1])
+        choose_multiple(15, [0.2, 0.2, 0.1, 0.2, 0.2, 0.1])
+        choose_multiple(16, [0.1, 0.2, 0.1, 0.3, 0.2, 0.1])
+        choose_multiple(17, [0.25, 0.25, 0.25, 0.25])
+        choose_multiple(18, [0.3, 0.3, 0.3, 0.1])
+        choose_multiple(19, [0.2, 0.1, 0.3, 0, 0.1, 0.2, 0.1], 3)  # 非相同概率时，没必要用 exclude。
+        choose_multiple(20)  # 各项均为相同概率，可省略不写。
+        choose_one(21, [0.2, 0.4, 0.3, 0.1])
+    except NoSuchElementException:
+        logging.error("任务执行失败，请检查配置。")
 
-def choose_multiple(question_number, question_probabilty, restrict = 10000):
-    number_of_choices = len(driver.find_elements(By.XPATH,f"//*[@id=\"div{question_number}\"]/div[2]/div"))
-    chosen_number = np.random.choice(list(range(1,number_of_choices+1)),p = question_probabilty, size = random.randint(1,min(restrict,number_of_choices)), replace = False)
+
+def probabilities_generator(choices_num, exclude=None):
+    if isinstance(exclude, list):
+        choices_num -= len(exclude)
+    probability = 1 / choices_num
+    res = [probability for i in range(choices_num)]
+    if exclude:
+        for i in exclude:
+            res.insert(i, 0)
+    return res
+
+
+def choose_one(question_number, question_probability=None, exclude=None):
+    """
+    :param question_number: int
+    :param question_probability: [] # If you set it None, It will auto generate an averages list.
+    :param exclude: [] # A list which question index you want to exclude.
+    """
+    el_choices = driver.find_elements(By.XPATH, f"//*[@id=\"div{question_number}\"]/div[2]/div")
+    choices_num = len(el_choices)
+    if not question_probability:
+        question_probability = probabilities_generator(choices_num, exclude=exclude)
+    chosen_number = np.random.choice(
+        a=list(range(1, choices_num + 1)),
+        p=question_probability
+    )
+    el_checked = driver.find_element(By.XPATH, f"//*[@id=\"div{question_number}\"]/div[2]/div[{chosen_number}]")
+    el_checked.click()
+
+
+def choose_multiple(question_number, question_probability=None, restrict=10000, exclude=None):
+    el_options = driver.find_elements(By.XPATH, f"//*[@id=\"div{question_number}\"]/div[2]/div")
+    choices_num = len(el_options)
+    if not question_number:
+        question_probability = probabilities_generator(choices_num, exclude=exclude)
+    chosen_number = np.random.choice(
+        a=list(range(1, choices_num + 1)),
+        p=question_probability,
+        size=random.randint(1, min(restrict, choices_num)),
+        replace=False
+    )
     for i in chosen_number:
-        driver.find_element(By.XPATH,f"//*[@id=\"div{question_number}\"]/div[2]/div[{i}]").click()
+        driver.find_element(By.XPATH, f"//*[@id=\"div{question_number}\"]/div[2]/div[{i}]").click()
 
 
-try:
-    driver.get("https://www.wjx.cn/vm/wE5Js0M.aspx")
-    choose_one(1, [0.5,0.5])
-    choose_one(2, [0.1,0.2,0.5,0.2])
-    choose_one(3, [0.6,0.3,0.1])
-    choose_one(4, [0.4,0.1,0.2,0.1,0.2])
-    choose_one(5, [0.2,0.2,0.1,0.3,0.2])
-    choose_one(6, [0.25,0.25,0.25,0.25])
-    choose_one(7, [0.3,0.2,0.1,0.4])
-    choose_one(8, [0.2,0.6,0.2])
-    choose_multiple(9, [0.3,0.25,0.2,0.125,0.125], 3)
-    choose_multiple(10, [0.3,0.25,0.2,0.15,0.1])
-    choose_one(11, [0.1,0.1,0.2,0.6])
-    choose_one(12, [0.2,0.8])
-    choose_multiple(13, [0.4,0.2,0.3,0.1], 2)
-    choose_multiple(14, [0.3,0.3,0.3,0.1])
-    choose_multiple(15, [0.2,0.2,0.1,0.2,0.2,0.1])
-    choose_multiple(16, [0.1,0.2,0.1,0.3,0.2,0.1])
-    choose_multiple(17, [0.25,0.25,0.25,0.25])
-    choose_multiple(18, [0.3,0.3,0.3,0.1])
-    choose_multiple(19, [0.2,0.1,0.1,0.2,0.1,0.2,0.1],3)
-    choose_multiple(20, [0.25,0.25,0.25,0.25])
-    choose_one(21, [0.2,0.4,0.3,0.1])
+def slider_move(loop_index, dest=380):
+    """
+    :param loop_index: int
+    :param dest: int # A position where you want to move.
+    """
+    try:
+        el_slider = WebDriverWait(driver, 10).until(
+            presence_of_element_located(
+                (By.XPATH, "//*[@id='nc_1__scale_text']/span"))
+        )
+        ActionChains(driver).click_and_hold(el_slider).perform()
+        ActionChains(driver).move_by_offset(xoffset=dest, yoffset=0).perform()
+        ActionChains(driver).release().perform()
+    except (TimeoutException, ElementClickInterceptedException):
+        logging.error(f"第{loop_index}次请求执行失败！")
 
-    driver.find_element_by_xpath('//*[@id="ctlNext"]').click()
-    time.sleep(0.5)
-    driver.find_element_by_xpath('//*[@id="alert_box"]/div[2]/div[2]/button').click()
-    driver.find_element_by_xpath('//*[@id="rectMask"]').click()
-    check = WebDriverWait(driver, 15).until(ec.invisibility_of_element_located((By.XPATH, "//*[@id=\"rectMask\"]")))
-    
-except:
-    pass
 
-driver.close()
+def main():
+    try:
+        for i in range(loop_count):
+            driver.get(question_url)
+            try:
+                driver.find_element(By.XPATH, '//*[@id="confirm_box"]/div[2]/div[3]/button[1]').click()  # 取消提示
+            except NoSuchElementException:
+                pass
+            choose_answer()
+            driver.find_element(By.XPATH, '//*[@id="ctlNext"]').click()
+            time.sleep(0.5)
+            driver.find_element(By.XPATH, '//*[@id="alert_box"]/div[2]/div[2]/button').click()
+            driver.find_element(By.XPATH, '//*[@id="rectMask"]').click()
+            print(f"第 {i} 次任务执行成功。")
+            try:
+                WebDriverWait(driver, 15).until(
+                    ec.url_changes(question_url)
+                )
+            except TimeoutException:
+                slider_move(i, dest=380)  # 若验证码逃逸失败，请自行调教参数 dest
+    except Exception:
+        logging.error("任务执行错误，正在退出任务。")
+    finally:
+        driver.close()
+        input()
+
+
+if __name__ == '__main__':
+    question_url = conf.QUESTION_URL or input("请输入问卷地址：")
+    loop_count = conf.LOOP_COUNT or int(input("请输入填写次数："))
+    opt = webdriver.ChromeOptions()
+    opt.add_experimental_option('excludeSwitches', ['enable-automation'])
+    opt.add_experimental_option('useAutomationExtension', False)
+    # driver = webdriver.Chrome(executable_path=r'./chromedriver', options=opt)
+    # 库主估计是 Linux 党，鉴于大多数学生党使用 Windows，为了降低使用门槛，注释了该行。
+    driver = webdriver.Chrome(options=opt)  # Windows下，将对应版本的 chromedriver 放置在 python 根目录，默认启用该行。
+    driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',
+                           {'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'})
+    np.random.seed(int(time.time()))
+    main()
